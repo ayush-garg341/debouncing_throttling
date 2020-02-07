b58ac1c5d60c9d3080d4bfa0a79f8b49238702c3
ayush-garg341 <gargayush341@gmail.com>
2020-02-07 14:10:16 +0530

    code for throttling added through descriptive UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Debounce from './debounce';
+import Throttle from './throttle';
+
 
 export class App extends Component {
 
   render(){
     return (
-      <div>
+      <div style={{backgroundColor:"grey"}}>
         <Debounce/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <Throttle/>
       </div>
     );
   }
--- src/throttle.js
+++ src/throttle.js
@@ -1,46 +1,42 @@
 import React, { Component } from 'react';
 import {Row, Col, Button, Label, Input} from 'reactstrap';
 
-export class Debounce extends Component {
+export class Throttle extends Component {
     constructor(props){
         super(props);
         this.state={
             sessionRemainingSeconds: 5,
-            running: false,
-            reset_timer: false,
+            running: true,
         }
         this.showAlertText = this.showAlertText.bind(this)
         this.debounce = this.debounce.bind(this)
-        this.showTimer = this.showTimer.bind(this)
         this.resetTimer = this.resetTimer.bind(this)
         this.startStopTimer = this.startStopTimer.bind(this)
+        this.enableInputBox = this.enableInputBox.bind(this)
     }
 
-    showTimer(){
-        // console.log("key is pressed")
-        var text = document.getElementById("some_text").value;
-        if(text!==''){
-            // console.log(text)
-            this.setState({running: true, reset_timer:true}, ()=>{this.resetTimer();})
-        }
-        else if (text===''){
-            this.setState({running: false}, ()=>{this.startStopTimer()})
-        }
+    enableInputBox(){
+        document.getElementById('throttle_text').removeAttribute('disabled')
     }
 
     debounce(text){
-        // alert(text);
-        document.getElementById("repeated_text").innerText = text
+        document.getElementById("repeated_throttle_text").innerText = text
+        if(text!=''){
+            document.getElementById('throttle_text').setAttribute('disabled', true)
+            clearTimeout(this.timer);
+            this.resetTimer();
+            this.timer = setTimeout(()=> this.enableInputBox(), 5000)
+            this.startStopTimer();
+        }
     }
 
     showAlertText(){
-        clearTimeout(this.timer);
-        this.text = document.getElementById("some_text").value;
-        this.timer = setTimeout(()=> this.debounce(this.text), 5000)
+        this.text = document.getElementById("throttle_text").value;
+        this.debounce(this.text);
     }
 
     resetTimer(){
-        this.setState({sessionRemainingSeconds:5}, ()=>{this.startStopTimer()})
+        this.setState({sessionRemainingSeconds:5})
     }
 
     startStopTimer(){
@@ -57,8 +53,6 @@ export class Debounce extends Component {
         }
     }
 
-
-
     render(){
         return(
             <div>
@@ -66,7 +60,7 @@ export class Debounce extends Component {
                     <Col sm='6'>
                     </Col>
                     <Col sm='6' >
-                    <b>Debouncing</b>
+                    <b>Throttling</b>
                     </Col>
                 </Row>
                 <Row >
@@ -74,7 +68,7 @@ export class Debounce extends Component {
                     <Row>
                         <Col sm='1'></Col>
                         <Col sm="5">
-                            <Input type="text" id='some_text' onChange={this.showAlertText} onKeyUp={this.showTimer}></Input>
+                            <Input type="text" id='throttle_text' onChange={this.showAlertText}></Input>
                         </Col>
                         <Col sm="1">
                             <h1>{this.state.sessionRemainingSeconds}</h1>
@@ -83,23 +77,23 @@ export class Debounce extends Component {
                     <Row>
                         <Col sm="1"></Col>
                         <Col sm="4">
-                            Text you type in above input box will be <b>displayed in red after 5 sec.</b> But if before 5 sec you press another key, then <b>delay restarts.</b>
+                            Text you type in above input box will be <b>displayed in red immediately.</b>But nothing will happen for the next 5 sec.<br/>
+                            <b>You can fire an event only once in throttle period.</b>
                         </Col>
                     </Row>
                     <Row>
                         <Col sm="1"></Col>
                         <Col>
-                            <Label id="repeated_text" style={{color:"red"}}></Label>
+                            <Label id="repeated_throttle_text" style={{color:"red"}}></Label>
                         </Col>
                     </Row>
                     </Col>
                     <Col sm="5">
-                    Debouncing in JavaScript is a practice used to <b>improve browser performance.</b><br/>
-                    There might be some functionality in a web page which requires <b>time-consuming computations.</b><br/>
-                    If such a method is invoked frequently, it might greatly affect the performance of the browser.<br/>
-                    It limits the rate at which a function gets invoked.<br/>
-                    If we are <b>invoking for the first time, our function will execute at the end of our delay.</b><br/>
-                    If we invoke and then <b>invoke again before the end of our delay, the delay restarts.</b>
+                    Throttle can be a little taxing as its desired behaviour has different interpretations.<br/>
+                    <b>Let’s start by limiting the rate at which we execute a function.</b><br/>
+                    The first call to our function will execute and sets the limit period<br/>
+                    <b>We can call our function during this period but it will not fire until the limit period has passed.</b><br/>
+                    If we invoke and then <b>invoke again before the end of our limit period, nothing happens</b>
                     </Col>
                 </Row>
             </div>
@@ -108,5 +102,5 @@ export class Debounce extends Component {
 }
 
 
-export default Debounce;
+export default Throttle;
 
